4473c94947cb926c561dc4850e98657b64625ccc
@Venom1904 <vaibhavjaiswalvj19@gmail.com>
2024-12-29 03:39:10 +0530

    completed the list functionality

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import os
 from dotenv import load_dotenv
 import requests
 from openai import OpenAI
+from plyer import notification
 
 load_dotenv()
 NEWS_API_KEY = os.getenv("NEWS_API_KEY")
@@ -72,6 +73,9 @@ def addTask(listName):
         except Exception as e:
             print("Error; {0}".format(e))
 
+# Function to read task inside the list
+# def readTask(listName):
+
 
 # Function to handle general commands with the help of openai
 def handleAiProcess(command):
@@ -93,60 +97,88 @@ def handleAiProcess(command):
 # Function to process the input command.
 def processCommand(c):
     # print(c)
-    if "open google" in c.lower():
-        webbrowser.open("https://google.com")
-    elif "open instagram" in c.lower():
-        webbrowser.open("https://instagram.com")
-    elif "open spotify" in c.lower():
-        webbrowser.open("https://spotify.com")
-    elif "open youtube" in c.lower():
-        webbrowser.open("https://youtube.com")
-    elif "open linkedin" in c.lower():
-        webbrowser.open("https://linkedin.com")
-    elif "play" in c.lower():
-        song = " ".join(c.lower().split(" ")[1:])
-        closest_match = findClosestSong(song, musicLibrary.music)
-        if closest_match:
-            link = musicLibrary.music[closest_match]
-            webbrowser.open(link)
-            speak(f"Playing {closest_match}")
+    try:
+        if "open google" in c.lower():
+            webbrowser.open("https://google.com")
+        elif "open instagram" in c.lower():
+            webbrowser.open("https://instagram.com")
+        elif "open spotify" in c.lower():
+            webbrowser.open("https://spotify.com")
+        elif "open youtube" in c.lower():
+            webbrowser.open("https://youtube.com")
+        elif "open linkedin" in c.lower():
+            webbrowser.open("https://linkedin.com")
+        elif "play" in c.lower():
+            song = " ".join(c.lower().split(" ")[1:])
+            closest_match = findClosestSong(song, musicLibrary.music)
+            if closest_match:
+                link = musicLibrary.music[closest_match]
+                webbrowser.open(link)
+                speak(f"Playing {closest_match}")
+            else:
+                speak(f"Sorry, I couldn't find {song} in yout library.")
+                print(f"Error: {song} not found in the music library.")
+
+        elif "news" in c.lower():
+            r = requests.get(url)
+            if r.status_code == 200:
+                data = r.json()
+
+                #Extract all articles
+                articles = data.get('articles', [])
+
+                #Speak the healines
+                for article in articles[:10]:
+                    speak(article['title'])
+
+        elif "create" in c.lower() and "list" in c.lower():
+            listName = getListName(c)
+            with open (f"{listName}.txt", "w") as file:
+                file.write(f"{listName} list\n")
+            addTask(listName)
+
+        elif "add" in c.lower() and "list" in c.lower():
+            listName = getListName(c)
+            addTask(listName)
+
+        elif "read" in c.lower() and "list" in c.lower():
+            listName = getListName(c)
+            with open (f"{listName}.txt") as file:
+                speak(file.read())
+
+        elif "show" in c.lower() and "list" in c.lower():
+            listName = getListName(c)
+            with open (f"{listName}.txt", "r") as file:
+                tasks = file.read()
+            notification.notify(
+                title=f"{listName} List",
+                message=tasks
+            )
+
+        elif "clear" in c.lower() and "list" in c.lower():
+            listName = getListName(c)
+            with open (f"{listName}.txt", "w") as file:
+                file.write("")
+            speak(f"{listName} list has been cleared succesfully")
+
+        elif "delete" in c.lower() and "list" in c.lower():
+            listName = getListName(c)
+            fileName = f"{listName}.txt"
+            try:
+                os.remove(fileName)
+                speak(f"{listName} list has been deleted succesfully")
+            except:
+                speak(f"{listName} list does not exists.")    
+
         else:
-            speak(f"Sorry, I couldn't find {song} in yout library.")
-            print(f"Error: {song} not found in the music library.")
+            # Pass the command to openAi
+            # output = handleAiProcess(c)
+            # print(output)
+            # speak(output)
+            pass
 
-    elif "news" in c.lower():
-        r = requests.get(url)
-        if r.status_code == 200:
-            data = r.json()
-
-            #Extract all articles
-            articles = data.get('articles', [])
-
-            #Speak the healines
-            for article in articles[:10]:
-                speak(article['title'])
-
-    elif "create" in c.lower() and "list" in c.lower():
-        listName = getListName(c)
-        with open (f"{listName}.txt", "w") as file:
-            file.write(f"{listName} list\n")
-        addTask(listName)
-
-    elif "add" in c.lower() and "list" in c.lower():
-        listName = getListName(c)
-        addTask(listName)
-
-    elif "add" in c.lower() and "list" in c.lower():
-        pass
-
-    # elif: show notification of the list created.
-
-    else:
-        # Pass the command to openAi
-        output = handleAiProcess(c)
-        print(output)
-        speak(output)
-
+    except Exception as e:
+        print("Error; {0}".format(e))    
 
 # The program begins from here
 if __name__ == "__main__":
